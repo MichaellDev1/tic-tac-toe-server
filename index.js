34abb83620f993eac5194f08eaacdb4f2a2240b9
MichaellDev1 <santuchomichael131@gmail.com>
2023-06-02 15:03:58 -0300

    Add file vercel

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ const io = new Server(server, {
 const rooms = {};
 
 io.on("connection", (socket) => {
-  console.log("User Connected", socket.id);
-
   socket.on("joinRoom", (room) => {
     socket.join(room);
-
     if (rooms[room]) {
+      //Verifica si la sala esta llena o no, en caso de serlo emitira y eliminara al usuario que se unio a la sala.
       if (rooms[room].length == 2) {
+        socket.leave(room);
         return socket.emit("notAuthorization", "not");
       } else {
         rooms[room] = [...rooms[room], { socketID: socket.id, turn: "o" }];
@@ -29,21 +28,39 @@ io.on("connection", (socket) => {
       rooms[room] = [{ socketID: socket.id, turn: "x" }];
       socket.emit("turnSelected", "x");
     }
+
+    //Emite si el juego es posible empezar o no.
     socket.to(room).emit("startGame", room);
   });
 
+  //Verifica si en la sala existen 2 usuarios, en caso de no se asi, no emitira el start
   socket.on("verify", (room) => {
     if (rooms[room].length == 2) {
       socket.to(room).emit("start", "start");
     }
   });
 
+  socket.on("resetGame", (room) => {
+    socket.broadcast.to(room).emit("resetGame", "reset");
+  });
+
+  //Crea y emite un mensaje globalmente
+  socket.on("createMessageGlobal", (message) => {
+    socket.broadcast.emit("newMessageGlobal", message);
+  });
+
+  //Envia un mensaje a la sala
+  socket.on("createMessageSingle", (message, room) => {
+    socket.broadcast.to(room).emit("newMessageSingle", message);
+  });
+
+  //Inicia el juego
   socket.on("playGame", ({ room }) =>
     socket.broadcast.to(room).emit("updateGame", "Hola")
   );
 
+  //Emite que turno es el siguiente, en caso de escuchar un click
   socket.on("gameBoard", (idRoom, newSquares) => {
-    console.log(rooms[idRoom]);
     socket.broadcast
       .to(idRoom)
       .emit(
@@ -53,6 +70,7 @@ io.on("connection", (socket) => {
       );
   });
 
+  //Si un usuario se desconecta sera eliminado de la sala y en caso de que los dos oponents se desconecten se eliminara la sala por completo.
   socket.on("disconnect", () => {
     Object.keys(rooms).forEach((room) => {
       if (rooms[room].length == 2) {
@@ -60,10 +78,15 @@ io.on("connection", (socket) => {
           (id) => id.socketID == socket.id
         );
 
-        console.log(verifyInx);
         if (verifyInx != -1) {
-          const deletePlayer = rooms[room].splice(verifyInx, 1);
+          const deletePlayer = rooms[room].filter(
+            (id) => id.socketID !== socket.id
+          );
+
+          deletePlayer[0].turn = "x";
+
           rooms[room] = deletePlayer;
+
           socket.to(room).emit("disconnectUser", "userDisconnect");
         }
       } else {
